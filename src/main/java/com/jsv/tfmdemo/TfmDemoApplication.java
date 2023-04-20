a042de159f98f47f681cef26564eccb1cab9ad18
Juanmanuelsanvi <juanmanuelsanvi@gmail.com>
2023-04-20 19:19:34 +0200

    primer commit

---
+++ src/main/java/com/jsv/tfmdemo/TfmDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jsv.tfmdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfmDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfmDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsv/tfmdemo/TfmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsv.tfmdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfmDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
